6bd23303b4954f338abb717f06671affd432bdc6
smitkalsariya <smitkalsariya13@gmail.com>
2024-09-25 20:27:16 +0530

    hero

--- src/components/home/herobanner/index.jsx
+++ src/components/home/herobanner/index.jsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
 import Img from '../../../assets/image/home.webp';
 import './herobanner.scss';
+
 export default function HeroBanner() {
+    useEffect(() => {
+        const link = document.createElement('link');
+        link.rel = 'preload';
+        link.as = 'image';
+        link.href = Img;
+        document.head.appendChild(link);
+
+        return () => {
+            document.head.removeChild(link);
+        };
+    }, []);
 
     return (
         <div>
             <div className='banner'>
-                <img src={Img} alt="Home banner"></img>
+                <img src={Img} alt="Home banner" />
             </div>
         </div>
-    )
+    );
 }

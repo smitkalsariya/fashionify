fcba6783a42734dd680ccda5a8191d18f67b3c76
smitkalsariya <smitkalsariya13@gmail.com>
2024-09-25 20:30:16 +0530

    app

--- src/components/home/herobanner/index.jsx
+++ src/components/home/herobanner/index.jsx
@@ -1,20 +1,7 @@
-import { useEffect } from 'react';
 import Img from '../../../assets/image/home.webp';
 import './herobanner.scss';
 
 export default function HeroBanner() {
-    useEffect(() => {
-        const link = document.createElement('link');
-        link.rel = 'preload';
-        link.as = 'image';
-        link.href = Img;
-        document.head.appendChild(link);
-
-        return () => {
-            document.head.removeChild(link);
-        };
-    }, []);
-
     return (
         <div>
             <div className='banner'>
